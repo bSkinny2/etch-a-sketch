609cad69089844205811313907c2f0970844c553
bskinny2 <117346451+bSkinny2@users.noreply.github.com>
2023-06-29 14:31:05 +0200

    JS creates 16x16 canvas

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var canvas = document.querySelector("div.canvas");
-var grid = 16;
+var cellsNumber = 16
 
-const pixel = document.createElement("div");
-canvas.appendChild(pixel);
+function createCanvas(cellsNumber) {
+
+    for (i=1; i < cellsNumber; i++) {
+        const columns = document.createElement("div");
+        columns.classList.add("columns");
+        canvas.appendChild(columns);
+
+        for (j=1; j< cellsNumber; j++) {
+            const rows = document.createElement("div");
+            rows.classList.add("rows");
+            columns.appendChild(rows);
+            }
+    }
+}
+
+createCanvas(cellsNumber)
+
