833232aa77b1443b5d9cc22ad9acc1fe5ce4651b
bskinny2 <117346451+bskinny2@users.noreply.github.com>
2023-07-01 08:39:04 +0200

    mouse click&drag draws on canvac

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var canvas = document.querySelector("div.canvas");
 var cellsNumber = 16
 let buttonErase = document.querySelector("#eraseButton")
 
+let mouseDown = false;
+
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function createCanvas(cellsNumber) {
 
@@ -14,11 +18,30 @@ function createCanvas(cellsNumber) {
             const rows = document.createElement("div");
             rows.classList.add("rows");
             columns.appendChild(rows);
-            rows.addEventListener('mouseover', () => rows.style.backgroundColor ="black")
+            rows.addEventListener('mousedown', draw);
+            rows.addEventListener('mouseover', draw);
+
+            /*var promHover = new Promise(function(resolve,reject){
+                rows.addEventListener('mouseover',resolve);
+            });
+            var promClick = new Promise(function(resolve,reject){
+                rows.addEventListener('mousedown',resolve);
+            });
+            Promise.all([promClick,promHover]).then(function() {
+                rows.style.backgroundColor ="black"
+            })
+            */
             }
+            }    
+        }
+    
+function draw(e) {
+    if (e.type === 'mouseover' && mouseDown) {
+        e.target.style.backgroundColor = "black"
     }
 }
 createCanvas(cellsNumber);
+
 
 buttonErase.addEventListener('click', () => {
     let rows = document.getElementsByClassName("rows");
